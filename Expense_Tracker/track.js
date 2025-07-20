0e73cacf504b2b9ce457f5e8e423adb9ab939341
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2025-07-20 11:40:45 +0500

    Add files via upload

--- Expense_Tracker/track.js
+++ Expense_Tracker/track.js
@@ -1,44 +1,71 @@
-// track.js
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("expense-form");
   const nameInput = document.getElementById("expense-name");
   const amountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
-  const totalAmount = document.getElementById("total-amount");
+  const totalAmountDisplay = document.getElementById("total-amount");
 
-  let total = 0;
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || []; // for storing expenses objects with unique id , name and amount
+  let totalAmount = totalAmountCalculate();
+  renderElement();
+  updateTotal();
+  // console.log(expenses);
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const name = nameInput.value.trim();
-    const amount = parseInt(amountInput.value);
+    const amount = parseFloat(amountInput.value.trim());
+    if (name !== "" && amount > 0 && !isNaN(amount)) {
+      const newExpense = {
+        id: Date.now(),
+        name: name,
+        amount: amount,
+      };
+      expenses.push(newExpense);
+      saveLocalStorage();
+      renderElement();
+      updateTotal();
+      // Clearing the Input
 
-    if (name === "" || amount < 0 || isNaN(amount)) {
-      alert("Please Enter Valid Name and Amount");
       nameInput.value = "";
       amountInput.value = "";
     }
-    const li = document.createElement("li");
-    li.innerHTML = `
-    ${name} - $${amount.toFixed(2)}
-    <button class = "delete-btn">Delete</button>
-    `;
-    expenseList.appendChild(li);
-
-    // Delete Logic
-
-    li.querySelector(".delete-btn").addEventListener("click", () => {
-      total -= amount;
-      expenseList.removeChild(li);
-      updateTotal();
-    });
-    total += amount;
-    updateTotal();
-    form.reset();
   });
 
-  function updateTotal() {
-    totalAmount.textContent = total.toFixed(2);
+  function renderElement() {
+    //
+    expenseList.innerHTML = "";
+    expenses.forEach((expense) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+      ${expense.name} - $${expense.amount}
+      <button data-id = "${expense.id}">Delete</button>
+      `;
+      expenseList.appendChild(li);
+    });
   }
+
+  // ************************* Deleting the ExpenseList *****************************
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const elementID = e.target.getAttribute("data-id");
+      expenses = expenses.filter((expense) => expense.id != elementID);
+      saveLocalStorage();
+      renderElement();
+      updateTotal();
+    }
+  });
+
+  function totalAmountCalculate() {
+    // calculate the total amount using array of objects
+    return expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+  function saveLocalStorage() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+  function updateTotal() {
+    totalAmount = totalAmountCalculate();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+  console.log(totalAmount);
 });
